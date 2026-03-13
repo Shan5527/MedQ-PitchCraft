87c08b296750f8cf9a54fa103edb3f2514172964
Shanmugapriyan Manivannan <mspriyan55@gmail.com>
2026-03-13 11:26:21 +0000

    Use this image for every doctor or medical person 'https://www.kindpng.c

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'media.istockphoto.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.kindpng.com',
+      },
     ],
   },
 };
