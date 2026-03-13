d362c3e88c11ee0813207b56052c384af20f5471
Shanmugapriyan Manivannan <mspriyan55@gmail.com>
2026-03-13 23:33:41 +0000

    images are not visible

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -1,3 +1,5 @@
+import data from './placeholder-images.json';
+
 export type ImagePlaceholder = {
   id: string;
   description: string;
@@ -5,71 +7,4 @@ export type ImagePlaceholder = {
   imageHint: string;
 };
 
-export const placeholderImages: ImagePlaceholder[] = [
-    {
-      "id": "logo",
-      "description": "MedQ 3D Logo",
-      "imageUrl": "https://media.istockphoto.com/id/1321798308/vector/health-insurance-icon.jpg?s=612x612&w=0&k=20&c=i-kP-b1Y2i97uUv12D_3z5s7-M_s_7h6w9TzM9oD0-I=",
-      "imageHint": "3d health shield icon"
-    },
-    {
-      "id": "home-hero",
-      "description": "3D render of a doctor character in a telemedicine concept.",
-      "imageUrl": "https://media.istockphoto.com/id/1453229670/photo/telemedicine-concept-with-3d-render-doctor-character.jpg?s=612x612&w=0&k=20&c=0WJ1d21_8s-Yv2-AYh0UvIxgqB8SjVjKBg2xVcoPjV8=",
-      "imageHint": "3d doctor online"
-    },
-    {
-      "id": "hospital-illustration",
-      "description": "3D illustration of a hospital room with bed and medical equipment.",
-      "imageUrl": "https://media.istockphoto.com/id/1366116895/vector/3d-hospital-room-with-bed-and-medical-equipment.jpg?s=612x612&w=0&k=20&c=e40m5_k9VvnF2s_j6Gq4Y8iVtuWTb82vE1sV0EBRdgo=",
-      "imageHint": "3d hospital room"
-    },
-    {
-      "id": "user-avatar",
-      "description": "A 3D cartoon avatar of a smiling man.",
-      "imageUrl": "https://media.istockphoto.com/id/1413524223/vector/3d-cartoon-avatar-of-smiling-black-man.jpg?s=612x612&w=0&k=20&c=h7kQpS_z-Amk2I3k3myZTR9NNLPB9YJb62n3w3kSg1M=",
-      "imageHint": "3d man avatar"
-    },
-    {
-      "id": "expert-avatar-1",
-      "description": "3D cartoon avatar of a smiling woman.",
-      "imageUrl": "https://media.istockphoto.com/id/1413524220/vector/3d-cartoon-avatar-of-smiling-woman.jpg?s=612x612&w=0&k=20&c=225R5iIFa3-iicwx3x3Jo1Wq-pYl06Oo1qYh2N1i8gE=",
-      "imageHint": "3d woman avatar"
-    },
-    {
-      "id": "expert-avatar-2",
-      "description": "3D cartoon avatar of a smiling woman.",
-      "imageUrl": "https://media.istockphoto.com/id/1413524220/vector/3d-cartoon-avatar-of-smiling-woman.jpg?s=612x612&w=0&k=20&c=225R5iIFa3-iicwx3x3Jo1Wq-pYl06Oo1qYh2N1i8gE=",
-      "imageHint": "3d woman avatar"
-    },
-    {
-      "id": "expert-avatar-3",
-      "description": "3D cartoon avatar of a smiling woman.",
-      "imageUrl": "https://media.istockphoto.com/id/1413524220/vector/3d-cartoon-avatar-of-smiling-woman.jpg?s=612x612&w=0&k=20&c=225R5iIFa3-iicwx3x3Jo1Wq-pYl06Oo1qYh2N1i8gE=",
-      "imageHint": "3d woman avatar"
-    },
-    {
-      "id": "expert-avatar-4",
-      "description": "3D cartoon avatar of a smiling woman.",
-      "imageUrl": "https://media.istockphoto.com/id/1413524220/vector/3d-cartoon-avatar-of-smiling-woman.jpg?s=612x612&w=0&k=20&c=225R5iIFa3-iicwx3x3Jo1Wq-pYl06Oo1qYh2N1i8gE=",
-      "imageHint": "3d woman avatar"
-    },
-    {
-      "id": "blog-1",
-      "description": "A senior man having a check-up at the doctor's office.",
-      "imageUrl": "https://media.istockphoto.com/id/1418223405/photo/senior-man-having-a-check-up-at-the-doctors-office.jpg?s=612x612&w=0&k=20&c=t1y-iFBc_y9EVqRSDRyDk0YmQW01xLz4aWwS7X-yVb0=",
-      "imageHint": "senior doctor checkup"
-    },
-    {
-      "id": "blog-2",
-      "description": "A female doctor visiting a patient at home.",
-      "imageUrl": "https://media.istockphoto.com/id/1399335803/photo/a-female-doctor-is-visiting-a-patient-at-home.jpg?s=612x612&w=0&k=20&c=qXl8Q52W2sXG4sDqd_fPZgI3b23zN2f-M2v0cGuU8kQ=",
-      "imageHint": "doctor home visit"
-    },
-    {
-      "id": "blog-3",
-      "description": "A beautiful female doctor looking at the camera in her office.",
-      "imageUrl": "https://media.istockphoto.com/id/1325423161/photo/beautiful-female-doctor-looking-at-camera-in-the-office.jpg?s=612x612&w=0&k=20&c=g-wPjybXm-L-d8_fL5XvM2fChyqgttOadY5n_5hYj2Y=",
-      "imageHint": "female doctor"
-    }
-  ];
+export const placeholderImages: ImagePlaceholder[] = data.placeholderImages;
